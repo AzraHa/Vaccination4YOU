a356ac8628ec85d6508a304309cd90df6542fc64
Azra Hadžihajdarević <51314049+AzraHa@users.noreply.github.com>
2021-05-08 12:02:20 +0200

    navigacija vaccineFragmenta u zavisnosti od odgovora

--- app/src/main/java/com/example/projekat/VaccineFragment.kt
+++ app/src/main/java/com/example/projekat/VaccineFragment.kt
@@ -5,19 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.projekat.databinding.FragmentVaccineBinding
 
 
 class VaccineFragment : Fragment() {
 
     /*U zavisnosti od odgovora sa ovog fragmenta imamo dvije mogucnosti:
     * data fragment ili end fragment */
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vaccine, container, false)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentVaccineBinding>(
+            inflater, R.layout.fragment_vaccine, container, false)
+
+        binding.covidNextBId.setOnClickListener{view : View ->
+            val checkedId = binding.covidQRadioId.checkedRadioButtonId;
+
+            if (-1 != checkedId) {
+
+                var odgovor = 0
+
+                when (checkedId) {
+
+                    R.id.covid_da_id-> odgovor = 1
+                    R.id.covid_ne_id -> odgovor = 2
+
+                }
+
+                if(odgovor == 1 ){
+
+                    view.findNavController().navigate(R.id.action_vaccineFragment_to_endFragment)
+
+                }else {
+
+                    view.findNavController().navigate(R.id.action_vaccineFragment_to_dataFragment)
+
+                }
+            }
+        }
+        return binding.root
     }
-
-
 }
