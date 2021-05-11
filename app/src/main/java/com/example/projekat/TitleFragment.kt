e20428434a62be01f43231a2328f7e8c7e6799a2
rickboiiii <72922288+rickboiiii@users.noreply.github.com>
2021-05-11 04:32:43 +0200

    brojac ljudi, icona i uklonjeni viska fileovi

--- app/src/main/java/com/example/projekat/TitleFragment.kt
+++ app/src/main/java/com/example/projekat/TitleFragment.kt
@@ -2,27 +2,58 @@ package com.example.projekat
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
+import android.os.Looper
 import android.view.*
-import androidx.fragment.app.Fragment
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.projekat.databinding.FragmentTitleBinding
 import timber.log.Timber
+import java.text.NumberFormat
+import kotlin.random.Random
+
 
 class TitleFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
-                R.layout.fragment_title,container,false)
+    private var randomNUM = Random.nextInt(100000,10000000)
+    private lateinit var randomTV : TextView
+    var handler = Handler(Looper.getMainLooper())
+    private lateinit var runnable : Runnable
 
-        binding.startId.setOnClickListener { view : View ->
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title, container, false
+        )
+
+        binding.startId.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_secondFragment)
         }
         setHasOptionsMenu(true)
+
+        randomTV = binding.regVakcinaid
+        increase()
+
         return binding.root
+    }
+
+    private fun increase(){
+        randomNUM += Random.nextInt(0,10)
+        randomTV.text = NumberFormat.getIntegerInstance().format(randomNUM)
+        refresh()
+    }
+
+    private fun refresh() {
+        runnable = Runnable {
+            kotlin.run { increase() }
+        }
+        handler.postDelayed(runnable, 1000)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -32,7 +63,7 @@ class TitleFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return NavigationUI.
-        onNavDestinationSelected(item,requireView().findNavController())
+        onNavDestinationSelected(item, requireView().findNavController())
                 || super.onOptionsItemSelected(item)
     }
 
@@ -68,6 +99,7 @@ class TitleFragment : Fragment() {
     }
     override fun onDestroyView() {
         super.onDestroyView()
+        handler.removeCallbacks(runnable)
         Timber.i("onDestroyView called")
     }
     override fun onDetach() {
